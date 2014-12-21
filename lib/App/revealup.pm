76f0fd454987240ddcba950ca8c1e63d6079efc9
Yusuke Wada <yusuke@kamawada.com>
2014-12-21 22:32:08 +0000

    Import of YUSUKEBE/App-revealup-0.18.tar.gz from CPAN.
    gitpan-cpan-distribution: App-revealup
    gitpan-cpan-version:      0.18
    gitpan-cpan-path:         YUSUKEBE/App-revealup-0.18.tar.gz
    gitpan-cpan-author:       YUSUKEBE
    gitpan-cpan-maturity:     released

--- lib/App/revealup.pm
+++ lib/App/revealup.pm
@@ -3,7 +3,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = "0.17";
+our $VERSION = "0.18";
 
 1;
 __END__
@@ -20,11 +20,11 @@ App::revealup - HTTP Server application for viewing Markdown formatted text as s
 
 =head1 DESCRIPTION
 
-B<App::revealup> is a package of web application modules and commands to showing Markdown formatted text with the JavaScript library B<"reveal.js">. Markdown text will be converted like slide shows if you use these C<revealup> commands.
+B<App::revealup> is a web application module for giving Markdown-driven presentations. The C<revealup> command starts a local web server to serve the your markdown presentation file with B<reveal.js>. The presentation can be viewed in a web browser. The reveal.js library offers comprehensive presenting features such as slide transitions, speaker notes and more.
 
 =head2 Sample Markdown
 
-    ## This is Title
+    ## This is an H2 Title
     
     Description... The horizontal slide separator characters are '---'
     
